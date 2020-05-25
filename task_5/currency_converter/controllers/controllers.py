5ae7b18efb2a189fb92f97e2c0d0cf61483851b8
emil.kholod <emil.kholod@gmail.com>
2020-05-25 16:12:24 +0500

    Test redirect

--- task_5/currency_converter/controllers/controllers.py
+++ task_5/currency_converter/controllers/controllers.py
@@ -7,13 +7,18 @@ class CurrencyConverter(http.Controller):
     def index(self, **kw):
         return "Hello, world"
 
+    @http.route('/currencies/', auth='public', website=True)
+    def currencies(self, **kw):
+        return {
+            'type': 'ir.actions.act_url',
+            'url': '/eur/',
+            'target': 'self',
+            'res_id': self.id,
+        }
 
-#     @http.route('/currency_converter/currency_converter/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('currency_converter.listing', {
-#             'root': '/currency_converter/currency_converter',
-#             'objects': http.request.env['currency_converter.currency_converter'].search([]),
-#         })
+    @http.route('/currencies/eur/', auth='public', website=True)
+    def currencies(self, **kw):
+        return "Hello world"
 
 #     @http.route('/currency_converter/currency_converter/objects/<model("currency_converter.currency_converter"):obj>/', auth='public')
 #     def object(self, obj, **kw):
