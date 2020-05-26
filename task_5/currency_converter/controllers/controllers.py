35f164eb93c7b53bce01a7bfb1de1544d3be436d
emil.kholod <emil.kholod@gmail.com>
2020-05-26 11:56:30 +0500

    Test view

--- task_5/currency_converter/controllers/controllers.py
+++ task_5/currency_converter/controllers/controllers.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import http
+import json
 
 
 class CurrencyConverter(http.Controller):
@@ -13,10 +14,13 @@ class CurrencyConverter(http.Controller):
 
     @http.route('/currencies/eur/', auth='public', website=True)
     def currencies(self, **kw):
-        TableOfCosts = http.request.env['res.currency']
-        print('Hello world!!!')
-        print(TableOfCosts.search([]))
-        return "Hello world2"
+        CurrencyRate = http.request.env['res.currency.rate']
+        currency_rate = CurrencyRate.search([])
+        # print(currency_rate.read(['id', 'currency_id', 'rate']))
+        # return json.dumps(currency_rate.read(['id', 'currency_id', 'rate']))
+        return http.request.render('currency_converter.index',
+                                   {'currency_rates': currency_rate})
+
 
 #     @http.route('/currency_converter/currency_converter/objects/<model("currency_converter.currency_converter"):obj>/', auth='public')
 #     def object(self, obj, **kw):
